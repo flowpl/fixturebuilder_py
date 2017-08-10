ae7f3c95df416332ad6c0a1e20323fcb15341f29
Florian Plattner <me@florianplattner.de>
2017-08-10 15:19:12 +0200

    feat: switch packaging to pbr

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from distutils.core import setup
 setup(
-  name='fixturebuilder',
-  packages=['fixturebuilder'],
-  version='1.0',
-  description='Build fixtures from a single source',
-  author='Florian Plattner',
-  author_email='me@florianplattner.de',
-  license='MIT',
-  url='https://github.com/flowpl/fixturebuilder_py',
-  keywords=['testing', 'fixture', 'test data'],
+  setup_requires=['pbr>=3.1.1'],
+  pbr=True,
 )
