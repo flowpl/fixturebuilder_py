156a4bba04cf8874498dc41f198ae75af74fa006
Florian Plattner <me@florianplattner.de>
2017-08-10 12:35:18 +0200

    feat: update ci to automatically publish to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='fixturebuilder',
+  packages=['fixturebuilder'],
+  version='1.0',
+  description='Build fixtures from a single source',
+  author='Florian Plattner',
+  author_email='me@florianplattner.de',
+  license='MIT',
+  url='https://github.com/flowpl/fixturebuilder_py',
+  download_url='https://github.com/flowpl/mypackage/archive/0.1.tar.gz',
+  keywords=['testing', 'fixture', 'test data'],
+)
